82cc3cd18a09d8e756398bb097a33973b9f1c9f1
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-11-22 05:09:47 -0600

    Carousel updated

---
+++ src/component/SingleCarousel.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Marquee from "react-marquee-slider";
+import styled from "styled-components";
+import times from "lodash/times";
+
+const Height = styled.div`
+  position: relative;
+  width: 100%;
+  height: ${(props) => (props.height ? props.height + "px" : "auto")};
+`;
+
+const Box = styled.div`
+  padding: ${(props) => props.scale * 25}px;
+`;
+
+
+const FullWidth = styled.div`
+  width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+`;
+
+
+
+const SingleCarousel = ({images, direction}) => {
+
+
+  return (
+    <FullWidth>
+      <Height height={0}>
+        <Marquee
+          velocity={15}
+          direction= {direction}
+          minScale={0.7}
+          styles={{gap: "10px"}}
+        >
+          {times(7, String).map((id) => (
+            <Box key={`marquee-example-review-${id}`}>    
+                <div className="">
+                  <img src={images[id]} alt=""  className={`w-[155px] h-[108px] tablet:w-[206px] tablet:h-[144px] laptop:w-[286px] laptop:h-[200px] mr-3 tablet:mr-4 laptop:mr-6`} />
+                </div>
+            </Box>
+          ))}
+        </Marquee>
+      </Height>
+    </FullWidth>
+  );
+};
+
+// will end in a loop without React.memo
+export default React.memo((SingleCarousel));
--- src/sections/Carousel.js
+++ src/sections/Carousel.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 
 import Image1 from "./../assets/image1.png"
 import Image2 from "./../assets/image2.png"
@@ -13,49 +15,28 @@ import Image11 from "./../assets/image11.png"
 import Image12 from "./../assets/image12.png"
 import Image13 from "./../assets/image13.png"
 import Image14 from "./../assets/image14.png"
+import SingleCarousel from "../component/SingleCarousel";
 
 
-    // ImageSlider.js
-    import React, { useState, useEffect } from 'react';
-import styles from './Carousel.module.css' 
-    
-const Carousels = () => {
-   
- 
-    
-  const topImages = [Image1,Image2,Image3,Image4,Image5,Image6,Image7]
-  const bottomImages = [Image8,Image9,Image10,Image11,Image12,Image13,Image14]
+const topImages = [Image1,Image2,Image3,Image4,Image5,Image6,Image7]
+const bottomImages = [Image8,Image9,Image10,Image11,Image12,Image13,Image14]
 
-  const [position, setPosition] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      // Move the marquee content to the left
-      setPosition((prevPosition) => prevPosition - 1);
-    }, 5); // Adjust the speed as needed
-
-    return () => clearInterval(intervalId); // Cleanup on component unmount
-  }, []);
+const Carousel = () => {
 
 
   return (
     <div className='bg-[#F2F1E8]'>
-    <div className={`w-full laptop:max-w-[1440px] mx-auto overflow-hidden ${styles.marqueeSlider}`} >
-      <div className={`flex gap-[13px] tablet:gap-[18px] mb-5 tablet:mb-10 laptop:mb-[50px] ${styles.sliderContent}`}   style={{ transform: `translateX(${position}px)` }}>
-        {topImages.map(image => <img key={image} className={`w-[155px] h-[108px] tablet:w-[206px] tablet:h-[144px] laptop:w-[286px] laptop:h-[200px] ${styles.img}`} src={image} alt=""/>)}
-      </div>
-      <div className={`flex gap-[13px] tablet:gap-[18px] mb-5 tablet:mb-10 laptop:mb-[50px] ${styles.sliderContent}`}   style={{ transform: `translateX(${position}px)` }}>
-        {bottomImages.map(image => <img key={image} className={`w-[155px] h-[108px] tablet:w-[206px] tablet:h-[144px] laptop:w-[286px] laptop:h-[200px] ${styles.img}`} src={image} alt=""/>)}
-      </div>
-      {/* <div className='flex gap-[13px] tablet:gap-[18px]'>
-        {bottomImages.map(image => <img className='w-[155px] h-[108px] tablet:w-[206px] tablet:h-[144px] laptop:w-[286px] laptop:h-[200px]' src={image} alt=""/>)}
-      </div> */}
+    <div className={`w-full laptop:max-w-[1440px] mx-auto overflow-hidden`} >
+  
+<div className="flex flex-col gap-5 tablet:gap-10 laptop:gap-[50px]">
+<SingleCarousel images={topImages} direction="rtl" />
+<SingleCarousel images={bottomImages} direction="ltr" />
+</div>
+</div>
+</div>
 
-    </div>
+  );
+};
 
-        </div>
-
-  )
-}
-
-export default Carousels
+// will end in a loop without React.memo
+export default Carousel;
