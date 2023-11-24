6a75a733668ea2b41b1484a29d98693aa8a5b6ad
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-11-24 16:22:03 -0600

    Carousel pictures added

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -2,7 +2,7 @@
 const Hero = () => {
   return (
     <div className='bg-[#F2F1E8] '>
-       <div className='w-full laptop:max-w-[1152px] mx-auto px-4 tablet:px-6 laptop:px-8 xl:px-0 text-center pt-[40px] pb-[28px] tablet:pt-[80px] tablet:pb-[50px] laptop:pt-[100px] laptop:pb-[56px]'>
+       <div className='w-full laptop:max-w-[1152px] mx-auto px-4 tablet:px-6 laptop:px-8 xl:px-0 text-center py-[40px] tablet:py-[80px] laptop:py-[100px]'>
       <div className='w-[288px] tablet:w-[524px] laptop:w-[815px] mx-auto'>
       <p className='text-[32px] font-bold leading-[39px] tracking-[-1px] tablet:text-[48px] tablet:leading-[40px] laptop:text-[64px] laptop:leading-[72px] laptop:tracking-[-2px] text-[#2E2E27] mb-6'>Beautiful powerpoint presentation for your next project</p>
       <p className='text-[16px] leading-6 tablet:text-[20px] tablet:leading-[28px] laptop:text-[24px] laptop:leading-8 text-[#64645F] mb-[40px]'>Pptdesigner supports your business to design beautiful powerpoint slides for your next presentation to get the numbers</p>
