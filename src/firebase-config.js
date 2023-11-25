869e5b34869d30e6af5641b76de393a70389a617
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-11-25 09:41:01 -0600

    Payment Implementation

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,24 +8,17 @@ import {getStorage} from 'firebase/storage';
 
 
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId:  process.env.REACT_APP_PROJECT_ID,
-//   storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId:  process.env.REACT_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDU28sfqPfCGUuU3NXemz-DWKHkVS42J-k",
-  authDomain: "pptdesigner-9ccaf.firebaseapp.com",
-  projectId: "pptdesigner-9ccaf",
-  storageBucket: "pptdesigner-9ccaf.appspot.com",
-  messagingSenderId: "519267137891",
-  appId: "1:519267137891:web:a2053706e1f9cde1737b56",
-  measurementId: "G-DM4R26DTHT"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:  process.env.REACT_APP_PROJECT_ID,
+  storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:  process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID,
 };
+
+
 
 
 
