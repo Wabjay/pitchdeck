5b71b15fc2475588bb6ac303e237fd05df5c4340
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-12-08 10:42:17 -0600

    Update

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { getDocs, collection, orderBy, query } from "firebase/firestore";
 import { db } from "../firebase-config";
 import BlogCard from "../component/BlogCard";
+import MetadataComponent from "../component/Metadata";
+import Analytics from "../component/Analytics";
 
 const Blog = () => {
 
@@ -22,8 +24,17 @@ const Blog = () => {
     getPosts();
   }, []);
 
+  
+
+
   return (
     <div className="mt-[60px] w-full">
+<Analytics/>
+<MetadataComponent
+        title="Blog Post"
+        description="This is a dynamic description."
+      />
+
       {/* // Top Section */}
       <div className="bg-[#EEFCF5]">
         <div className="w-full laptop:max-w-[1152px] mx-auto px-4 tablet:px-6 laptop:px-8 xl:px-0 py-[40px] tablet:py-[80px] laptop:py-[100px]">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,16 @@ import Presentation from "../sections/Presentation";
 import Works from "../sections/Works";
 import Pricing from "../sections/Pricing";
 import Faq from "../sections/Faq";
+import MetadataComponent from "../component/Metadata";
 
 const Home = () => {
   return (
     <>
+    <MetadataComponent
+        title="PPTDesigner"
+        description="Pptdesigner supports your business to design beautiful powerpoint slides for your next presentation to get the numbers" 
+        page={undefined} image={undefined} tags={undefined}      />
+
       <div className="mt-[60px]">
         <Hero />
         <Carousel />
